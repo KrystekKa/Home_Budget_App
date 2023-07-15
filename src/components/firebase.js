2261be386a567d9c114aae63a86a6cab9b329d10
KrystekKa <121402187+KrystekKa@users.noreply.github.com>
2023-07-15 14:07:26 +0200

    login and singin form

---
+++ src/components/firebase.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword } from "firebase/auth";
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDj511XSb_P3L4-0atAgEBpCeAj7fNTAwI",
+    authDomain: "homebudget-874d0.firebaseapp.com",
+    projectId: "homebudget-874d0",
+    storageBucket: "homebudget-874d0.appspot.com",
+    messagingSenderId: "163705650859",
+    appId: "1:163705650859:web:1499adf3d8c334d762c1ee",
+    measurementId: "G-JBZKRGZZ1S"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app)
+
+export { auth, signInWithEmailAndPassword, createUserWithEmailAndPassword };
