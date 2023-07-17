9f81031c141027613fa78d0fa8e9ccc2920691f6
KrystekKa <121402187+KrystekKa@users.noreply.github.com>
2023-07-17 20:26:06 +0200

    add_home_screen

---
+++ src/components/Home.js
@@ -0,0 +1,26 @@
+import '../css/App.scss';
+import React from 'react';
+import Menu from "./Menu";
+
+export default function Home() {
+    return (
+        <>
+        <Menu />
+            <div className={"home_title"}>
+              <h1 className={"budget"}>Dostępne środki</h1>
+              <h2 className={"budget_sum"}>2222 zł</h2>
+            </div>
+            <div className={"home_buttons"}>
+                <a href="#" className={"add_wallet btn"}>Dodaj portfel</a>
+                <a href="#" className={"add_expense btn"}>Dodaj wydatek</a>
+            </div>
+            <p className={"last_expense_title"}>Ostatnie wydatki</p>
+            <div className={"last_expense"}>
+                <ul className={"expenses"}>
+                    <li className={"expenses_error"}>Brak wydatków w ostatnim czasie</li>
+                    <li className={"expense"}>Kwota Kategoria Data</li>
+                </ul>
+            </div>
+        </>
+    );
+}
